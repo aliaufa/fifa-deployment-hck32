cb5dfd75af7c9d4ee9d6075f4855434ecd65d2aa
aliaufa <aliaufafakhri@gmail.com>
2025-10-07 14:08:46 +0700

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import eda, predict
+
+with st.sidebar:
+    st.title('Page Navigation')
+    # input
+    page = st.radio('Page', ['EDA', 'Model Demo'])
+
+    st.write('# About')
+    st.write('''
+             Page ini adalah informasi data dan demo dari 
+             model prediksi player rating
+             ''')
+    
+if page == "EDA":
+    eda.run()
+else:
+    predict.run()
+
